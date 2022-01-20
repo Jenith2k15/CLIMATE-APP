90af21ac18b1d41cf9c386ab1df9b10339ab8e60
wb582760 <jsaravanan@worldbank.org>
2022-01-20 19:47:33 +0530

    climate chart and register user completed

--- ClimateApp/ClimateApp/Controllers/HomeController.cs
+++ ClimateApp/ClimateApp/Controllers/HomeController.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using ClimateApp.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.IO;
+using Newtonsoft.Json;
+using ClimateApp.Models;
 
 namespace ClimateApp.Controllers
 {
@@ -23,8 +26,22 @@ namespace ClimateApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Register()
         {
+            Register register = new Register();
+            return View(register);
+        }
+
+        [HttpPost]
+        public IActionResult Register(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                string registerData = $"{register.FullName.ToLower()}'s bio is {register.Bio.ToLower()}\r";
+                // Write the json string to file.
+                System.IO.File.AppendAllText(System.IO.Path.Combine("data/register.txt"), registerData);
+                return RedirectToAction("Register");
+            }
             return View();
         }
 
---
+++ ClimateApp/ClimateApp/Models/Register.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClimateApp.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Fullname is required")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage = "Bio is required")]
+        public string Bio { get; set; }
+    }
+}
